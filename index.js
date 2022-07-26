f16963a1af1e16728535f8bdcc9beeeb998e3619
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-07-26 15:18:43 -0300

    build: extensions

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectToDatabase = require('./src/Database/database');
 
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+const port = process.env.PORT || 3002;
 
+const app = express();
 
-const port = process.env.PORT || 3002
+app.use(cors());
+app.use(express.json());
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
+connectToDatabase();
 
 app.listen(port, () =>
-  console.log(`Server listening on http://localhost:${port}`),
+	console.log(`Server listening on http://localhost:${port}`),
 );
