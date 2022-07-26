37140f67b5e43302a218307d811fb9c8ba03d87e
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-07-26 14:30:19 -0300

    build: starting api

---
+++ index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+
+const port = process.env.PORT || 3002
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(port, () =>
+  console.log(`Server listening on http://localhost:${port}`),
+);
