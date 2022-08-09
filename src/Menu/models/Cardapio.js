cb1aed66107de48c1ca15b177278f1a1f44c09a4
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-08-08 23:52:03 -0300

    push

--- src/Menu/models/Cardapio.js
+++ src/Menu/models/Cardapio.js
@@ -4,7 +4,7 @@ const mogoose = require('mongoose');
 const ItensSchema = new mogoose.Schema ({
     name:{type: String, required: true},
     description:{ type: String, required: true},
-    url:{type: String, required: true}
+    image:{type: String, required: true}
 
 });
 
